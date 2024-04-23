ace41cb8275c34a6e1fe88283a6242d91f5ec292
Koryts Vladislav <koryts@mail.ru>
2024-04-23 20:35:32 +0300

    Lesson №3

--- src/main/java/com/javarush/telegrambot/MyFirstTelegramBot.java
+++ src/main/java/com/javarush/telegrambot/MyFirstTelegramBot.java
@@ -11,6 +11,8 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.javarush.telegrambot.TelegramBotContent.*;
+
 public class MyFirstTelegramBot extends MultiSessionTelegramBot {
     public static final String NAME = "jru_test_student_bot"; // TODO: добавьте имя бота в кавычках
     public static final String TOKEN = "6874183100:AAFS3WxLEUmxE5W_hGu9T1kb6DAW6052xvQ"; //TODO: добавьте токен бота в кавычках
@@ -22,20 +24,53 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
     @Override
     public void onUpdateEventReceived(Update updateEvent) {
         // TODO: основной функционал бота будем писать здесь
-        if(getMessageText().equals("/bye")){
-            sendTextMessageAsync("Asta la vista, baby!");
-        }
-
         if (getMessageText().equals("/start")) {
-            sendTextMessageAsync("Ваше любимое животное?", Map.of("Кот", "cat", "Собака", "dog"));
+            setUserGlory(0);
+            sendTextMessageAsync(STEP_1_TEXT, Map.of("Взлом холодильника", "step_1"));
         }
 
-        if (getCallbackQueryButtonKey().equals("cat")) {
-            sendPhotoMessageAsync("step_4_pic");
+        if (getCallbackQueryButtonKey().equals("step_1")) {
+            addUserGlory(20);
+            sendTextMessageAsync(STEP_2_TEXT,
+               Map.of( "Взять сосиску! +20 славы", "step_2",
+                    "Взять рыбку! +20 славы", "step_2",
+                    "Взять пиво! +20 славы", "step_2"));
         }
 
-        if (getCallbackQueryButtonKey().equals("dog")) {
-            sendPhotoMessageAsync("step_6_pic");
+        if (getCallbackQueryButtonKey().equals("step_2")) {
+            addUserGlory(20);
+            sendTextMessageAsync(STEP_3_TEXT, Map.of("Взлом робота-пылесоса", "step_3"));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step_3")) {
+            addUserGlory(30);
+            sendTextMessageAsync(STEP_4_TEXT,
+                    Map.of( "Отправить робот пылесос за едой! +30 славы", "step_4",
+                            "Покататься на работе пылесосе! +30 славы", "step_4",
+                            "Убежать от робота пылесоса! +30 славы", "step_4"));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step_4")) {
+            addUserGlory(30);
+            sendTextMessageAsync(STEP_5_TEXT, Map.of("Надеть и включить GoPro!", "step_5"));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step_5")) {
+            addUserGlory(40);
+            sendTextMessageAsync(STEP_6_TEXT,
+                    Map.of( "Снять на видео как ты кушаешь рыбку! +30 славы", "step_6",
+                            "Покушать рыбку и снять это на видео! +30 славы", "step_6",
+                            "Насладиться рыбкой, снимая это на видео! +30 славы", "step_6"));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step_6")) {
+            sendTextMessageAsync(STEP_7_TEXT, Map.of("Взлом пароля на компьютере!", "step_7"));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step_7")) {
+            addUserGlory(50);
+            sendTextMessageAsync(STEP_8_TEXT);
+            sendTextMessageAsync(FINAL_TEXT);
         }
     }
 
